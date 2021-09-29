5422d1c5e2129698f03d63f67b189f15f9d70d6e
Rayan Freitas <72174813+Rayan-Freitas@users.noreply.github.com>
2021-09-29 12:44:00 -0300

    Update conexao.php

--- SiteCadastroConsultaClientes/conexao.php
+++ SiteCadastroConsultaClientes/conexao.php
@@ -1,8 +1,8 @@
 <?php 
-define('HOST', 'localhost');   //MUDAR PORTA QUANDO FOR CONFIGURAR
-define('USUARIO', 'u432591547_rayan');
-define('SENHA', 'Deniedofservice0401');
-define('DB', 'u432591547_clientes');
+define('HOST', '');   //MUDAR PORTA QUANDO FOR CONFIGURAR
+define('USUARIO', '');
+define('SENHA', '');
+define('DB', '');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
 
